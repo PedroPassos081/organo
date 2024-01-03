5b47564dad2507b6e6ca293865642741fee05bfc
PedroPassos081 <pedro.passos081@gmail.com>
2024-01-03 17:46:42 -0300

    Adicionando opção de deletar

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,15 +1,14 @@
 import Colaborador from '../Colaborador'
 import './time.css'
 
-const Time = ({ time, colaboradores }) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor}) => {
     return (
-
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+            <input  onChange={evento => mudarCor(evento.target.value)} value={time.corSecundaria} type='color' className='input-cor' />
             <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
-                    console.log(<Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={() => {}} />)
-                   return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={() => {}} />
+                   return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={aoDeletar} />;
                 })}
             </div>
         </section>
